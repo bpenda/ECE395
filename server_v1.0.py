510ca88052d692511c7f49abaf529ea96fefdf28
Bill <bpenda94@gmail.com>
2016-10-27 14:47:25 -0500

    server updates

--- server_v1.0.py
+++ server_v1.0.py
@@ -1,11 +1,12 @@
 #pip import pyown
 #pip import praw
 
-from time import gmtime, strftime
+from time import gmtime, strftime, sleep
 from subprocess import call
 import os.path
 import pyowm
 import praw
+import signal,sys
 
 WEATHER_API_KEY = "d3041fa4dec2792d29c137b03e0f5bd0"
 WEATHER_LOC = "Champaign, IL"
@@ -18,13 +19,14 @@ SUBREDDIT = "news"
 SCREEN_SESSION = "mirror"
 USB_PATH = "/dev/tty.usbserial-A50285BI"
 BT_PATH = "/dev/tty.HC-06-DevB"
-UART_PATH = BT_PATH
+UART_PATH = ''
 BAUD_RATE = 115200
 
-def connectUART():
-    call("screen -S " + SCREEN_SESSION + " -d -m " + UART_PATH + " " + str(BAUD_RATE), shell=True)
+def connectUART(path):
+    call("screen -S " + SCREEN_SESSION + " -d -m " + path + " " + str(BAUD_RATE), shell=True)
+    print 'sending to shell: '"screen -S " + SCREEN_SESSION + " -d -m " + path + " " + str(BAUD_RATE)
 def printUART(message):
-    call("screen -S " + SCREEN_SESSION + ' -p 0 -X stuff "' + message + '\n"', shell=True)
+    call("screen -S " + SCREEN_SESSION + ' -p 0 -X stuff "' + message + '"', shell=True)
 def disconnectUART():
     call("screen -S " + SCREEN_SESSION + " -X quit", shell=True)
 
@@ -49,15 +51,43 @@ def getInfo():
     info['Headlines'] = getHeadlines()
     return info
 
+def signal_handler(signal, frame):
+    print "exiting cleanly"
+    disconnectUART()
+    sys.exit(0)
+
 def server(): 
+
     if os.path.isfile(USB_PATH):
         UART_PATH = USB_PATH
     else:
         UART_PATH = BT_PATH
 
+    UART_PATH = BT_PATH
     print getInfo()
-    connectUART()
-    printUART("Hello world!")
+    signal.signal(signal.SIGINT, signal_handler)
+    connectUART(UART_PATH)
+    delay = 3
+    sleep(delay)
+    while 1:
+        printUART(getDateTime().split(' ')[0] +"\r")
+        print getDateTime().split(' ')[0]
+        sleep(delay)
+        printUART(getDateTime().split(' ')[1] +"\r")
+        print getDateTime().split(' ')[1]
+        sleep(delay)
+
+        printUART("cold\r")
+        sleep(delay)
+        printUART("gray\r")
+        sleep(delay)
+        printUART("death!\r")
+        sleep(delay)
+        printUART("famine!\r")
+        sleep(delay)
+        printUART("poverty!\r")
+        sleep(8)
     disconnectUART()
+    signal.pause()
 
 server()
