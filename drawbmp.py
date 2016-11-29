379bdcd7dd67ff9e096ba53b7dacd6e60e4f7105
hungle2 <hungle2@Illinois.edu>
2016-11-29 16:20:56 -0600

    Create drawbmp.py

---
+++ drawbmp.py
@@ -0,0 +1,11 @@
+import PIL
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+font = ImageFont.truetype("../Downloads/TNR.ttf",25)
+img = Image.new("RGBA", (480,800),(255,255,2))
+draw = ImageDraw.Draw(img)
+draw.text((0, 0),"This is a test",(255,255,0),font=font)
+draw = ImageDraw.Draw(img)
+draw = ImageDraw.Draw(img)
+img.save("a_test.png")
